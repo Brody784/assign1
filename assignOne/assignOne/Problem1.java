b772003d36e8989a7f6e20a08254d211db4b13f6
Brody Cauthen <student@debianVM.cs.uafs.edu>
2026-09-01 21:15:28 -0500

    Homework1

--- assignOne/assignOne/Problem1.java
+++ assignOne/assignOne/Problem1.java
@@ -1,16 +1,35 @@
 package assignOne;
 
-public class Problem1 {
+public class Problem1{
 
 	public static void main(String[] args) {
-		
+		int value;
+		value = stringToInt("-12345");
+		System.out.println(value);
 
-	}
-	
-	public static int stringToInt(String str) {
-		
-		
-		return 0;
-	}
+		}
+		public static int stringToInt(String str) {
+		int number = 0;
+		char zero = '0';
+		int index = 0;
+		int initial;
+		char sign = ' ';
+		if(str.charAt(0) == '-' || str.charAt(0) == '+') {
+		sign = str.charAt(0);
+		index++;
+		}else {
+		number = str.charAt(index) - zero ;
+		index++;
+		}
+		for( initial = 0; initial < str.length() - 1 ; initial++) {
+		number = 10 * number;
+		number += str.charAt(index ) - zero;
+		index++;
+		}
+		if(sign == '-') {
+		number = number * -1;
+		}
+		return number;
+		}
 
 }
--- assignOne/assignOne/Problem2.java
+++ assignOne/assignOne/Problem2.java
@@ -3,14 +3,42 @@ package assignOne;
 public class Problem2 {
 
 	public static void main(String[] args) {
-		
 
-	}
-	
-	public static String intToString(int value) {
-		
-		
-		return null;
-	}
+		int value = 123456;
+
+		String result = intToString(value);
+
+		System.out.println(result);
+		}
+
+		public static String intToString(int value) {
+
+		int num = 0;
+		int length = 0;
+		String result = "";
+		int percent = 10;
+		int digit;
+		int index;
+
+		while (num != value) {
+		length += 1;
+		percent *= 10;
+		num = value % percent;
+		}
+
+		percent = percent / 10;
+
+		for ( index = 0; index <= length; index++) {
+
+		digit = value / percent;
+		value = value % percent;
+
+		result += digit;
+
+		percent = percent / 10;
+		}
+
+		return result;
+		}
 
 }
