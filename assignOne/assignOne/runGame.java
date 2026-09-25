b2df235f700713c3e486d504790dfd7c4ed106ca
Brody Cauthen <student@debianVM.cs.uafs.edu>
2026-09-24 19:32:36 -0500

    Game work

---
+++ assignOne/assignOne/runGame.java
@@ -0,0 +1,27 @@
+package assignOne;
+
+public class runGame {
+	public static void main(String[] args) {
+
+		NextGame game = new NextGame();
+
+		game.displayIntro();
+		game.loadScores();
+		game.random();
+
+		while (!game.winner() && !game.gameOver()) {
+		System.out.println("=== turn " + game.turn() + "===");
+		game.userInput();
+		game.compare();
+		}
+		if(game.winner()) {
+		game.highScore();
+		game.saveScores();
+		}
+
+		game.displaySolution();
+		game.displayHighScores();
+		}
+
+
+}
